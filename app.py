7380a327d15e045d987653061c8cbda0334df6fc
alistairhmparker <alistairhmparker@gmail.com>
2026-02-21 12:16:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -394,6 +394,10 @@ def reset():
     session.clear()
     return redirect(url_for("index"))
 
+@app.get("/health")
+def health():
+    return "ok", 200
 
 if __name__ == "__main__":
+
     app.run(debug=True)
